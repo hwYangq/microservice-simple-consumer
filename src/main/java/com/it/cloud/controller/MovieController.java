f1585ae2841346a29adecb4fa754d339fc5e5680
yanghongwei <yanghw_117@163.com>
2019-02-23 19:25:37 +0800

    init

--- src/main/java/com/it/cloud/controller/MovieController.java
+++ src/main/java/com/it/cloud/controller/MovieController.java
@@ -1,13 +1,12 @@
 package com.it.cloud.controller;
 
+import com.it.cloud.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import com.it.cloud.entity.User;
 
 @RestController
 public class MovieController {
